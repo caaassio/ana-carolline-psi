79702054c35ac847ff7b5b6782c17a940530deb3
caaassio <caaassio@gmail.com>
2025-06-25 21:31:17 -0300

    queries-e-js-dos-posts

--- js/script-post.js
+++ js/script-post.js
@@ -11,3 +11,26 @@ document.addEventListener("DOMContentLoaded", function () {
         tempoElemento.innerHTML = ` ${tempoLeitura} min`;
     }
 });
+
+// ----------------- header loader ------------------------
+fetch("../header.html")
+          .then(response => response.text())
+          .then(data => document.getElementById("header-container").innerHTML = data);
+
+
+        fetch("../footer.html")
+          .then(response => response.text())
+          .then(data => document.getElementById("footer-container").innerHTML = data);
+
+// ------------------ Opacidade BTT  -----------------------------
+    window.addEventListener("scroll", function () {
+        let btt = document.querySelector(".btt");
+
+        if (btt) {
+            if (window.scrollY > 0) {
+                btt.style.opacity = "0.5";
+            } else {
+                btt.style.opacity = "1";
+            }
+        }
+    });
